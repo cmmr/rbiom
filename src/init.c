626c08be02c47ea652711884c3a23bcd6f6ceaec
Daniel Smith <dansmith@orst.edu>
2025-01-18 00:25:09 +0000

    native routine registration

---
+++ src/init.c
@@ -0,0 +1,30 @@
+/* Generated with tools::package_native_routine_registration_skeleton('.',,,FALSE) */
+
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+extern SEXP C_alpha_div(SEXP, SEXP, SEXP, SEXP);
+extern SEXP C_beta_div(SEXP, SEXP, SEXP, SEXP);
+extern SEXP C_pthreads(void);
+extern SEXP C_rarefy(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP C_read_tree(SEXP);
+extern SEXP C_unifrac(SEXP, SEXP, SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"C_alpha_div", (DL_FUNC) &C_alpha_div, 4},
+  {"C_beta_div",  (DL_FUNC) &C_beta_div,  4},
+  {"C_pthreads",  (DL_FUNC) &C_pthreads,  0},
+  {"C_rarefy",    (DL_FUNC) &C_rarefy,    6},
+  {"C_read_tree", (DL_FUNC) &C_read_tree, 1},
+  {"C_unifrac",   (DL_FUNC) &C_unifrac,   5},
+  {NULL, NULL, 0}
+};
+
+void R_init_rbiom(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
